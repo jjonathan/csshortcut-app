b817c9ce3e52e2b26983142c4cba85f5ea490ee9
Jonathan Machado <jonathan.mmachado@outlook.com>
2017-06-09 10:22:02 -0300

    partials e block content

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('stylus', function(){
 })
 
 gulp.task('watch', function(){
-    gulp.watch(['./src/*.pug'], ['pug'])
+    gulp.watch(['./src/**/*.pug'], ['pug'])
     gulp.watch(['./src/assets/styles/*.styl'], ['stylus'])
 })
 
