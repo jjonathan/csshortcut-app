c49b217c82d53a2ea9dcc3398052a3c410b060ad
Jonathan Machado <jonathan.mmachado@outlook.com>
2017-06-13 09:36:18 -0300

    Criando o header && ajustado o gulp.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,41 @@
-var gulp    = require('gulp')
-var pug     = require('gulp-pug')
-var stylus  = require('gulp-stylus')
-var connect  = require('gulp-connect')
+var gulp     = require('gulp');
+var pug      = require('gulp-pug');
+var stylus   = require('gulp-stylus');
+var connect  = require('gulp-connect');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('pug', function(){
     gulp.src('./src/*.pug')
         .pipe(pug())
         .pipe(gulp.dest('./out'))
         .pipe(connect.reload())
-})
+});
 
 gulp.task('stylus', function(){
     gulp.src('./src/assets/styles/*.styl')
         .pipe(stylus())
         .pipe(gulp.dest('./out/assets/styles/'))
         .pipe(connect.reload())
-})
+});
+
+gulp.task('imagemin', function(){
+    gulp.src('src/assets/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('out/assets/img/'))
+});
 
 gulp.task('watch', function(){
     gulp.watch(['./src/**/*.pug'], ['pug'])
     gulp.watch(['./src/assets/styles/*.styl'], ['stylus'])
-})
+    // gulp.watch(['./src/assets/img/*'], ['stylus'])
+});
 
 gulp.task('serve', function(){
     connect.server({
         root : './out',
         livereload: true
     })
-})
+});
 
-gulp.task('build', ['pug', 'stylus'])
-gulp.task('server', ['serve', 'watch'])
+gulp.task('build', ['pug', 'stylus', 'imagemin'])
+gulp.task('server', ['build', 'serve', 'watch'])
